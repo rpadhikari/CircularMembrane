a7a82b7c6a0bc58b4050c389a44c1729f4bfff4d
Rajendra Adhikari <rpadhikari@gmail.com>
2018-05-03 06:33:53 +0545

    better in shape, almost complete

--- membrane.f90
+++ membrane.f90
@@ -1,11 +1,19 @@
 program membrane
   implicit none
-  integer(4) i, j, k, m, n
-  real(8), external :: bessel
-  real(8) r, lambda(3,3), theta
-  real(8) A0, B0, C0, D0
-  real(8) c, t 
-  real(8), external :: u
-
-
+  integer(4) i, j, k
+  real(8) r, theta, t
+  real(8), external :: Umn
+  real(8), parameter :: pi=3.1415926535897930d0
+  open(4, file='out.data', action='write')
+  do i=1, 100
+    r=dbla(i)*0.10d0
+    do j=0, 628
+      theta=dble(j)*pi/180.0d0
+      do k=1, 100
+        t=dble(k)*0.01
+        write(4,12) r, theta, t, Umn(1,0, r, theta, t)
+      end do
+    end do
+  end do
+  12 format(f5.3, f5.3, f8.4, f20.15)
 end program membrane
