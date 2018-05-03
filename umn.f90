e71bfe9b9acb71cf54d1f2a95977ad4e43649d9f
Rajendra Adhikari <rpadhikari@gmail.com>
2018-05-03 05:58:23 +0545

    Add files via upload

--- umn.f90
+++ umn.f90
@@ -1,28 +1,41 @@
-real(8) function umn(m, n, r, theta, t)
+! This function calculates the displacement of vibration of circular
+! membrane using Bessel's function.
+! Author: Rajendra Adhikari
+! Date: May 3rd, 2018
+
+real(8) function Umn(m, n, r, theta, t)
   implicit none
-  integer(4) m, n
-  real(8) r, theta, t
-  real(8) A0, B0, C0, D0
-  real(8) c, nrr, rho, height
-  real(8) amn(4, 4), lambda(4, 4)
-  real(8), external :: bessel
+  integer(4) i, j ! running index variable
+  integer(4) m, n ! m is the order of Bessel function and n is of root
+  real(8) r, theta, t ! Radius, angle and time variables
+  real(8) A0, B0, C0, D0 ! normalizing coefficients
+  real(8) c ! velocoty calculated as below formula
+  real(8) nrr ! surface tension
+  real(8) rho ! density of the material
+  real(8) height ! thickness of the membrane
+  real(8) amn(4, 4) ! roots of Bessel function, reading from file roots.data
+  real(8) lambda(4, 4) ! root divided by radius of membrane
+  real(8) radius ! radius of circular membrane 
+  real(8), external :: bessel ! Bessel function defined earlier
   
-  open(1, file='in.data', sction='read', status='old')
-  open(2, file='root.data', sction='read', status='old')
-  read(1,*) nrr
+  open(1, file='in.data', action='read', status='old') ! membrane parameters
+  open(2, file='root.data', action='read', status='old') ! roots file
+  read(1,*) nrr 
   read(1,*) rho
   read(1,*) height
   read(1,*) A0, B0, C0, D0
   read(1,*) radius
   close(1)
-  c=dsqrt(nrr/(rho*height))
+  ! calculation of transverse speed of wave in membrane
+  c=dsqrt(nrr/(rho*height)) ! velocity
   do i=1, 4
-    read(2,*) (amn(i, j), j=1,4)
+    read(2,*) (amn(i, j), j=1,4) ! roots from file roots.data 4x4 only
   end do
   lambda=amn/radius
   
-  umn=(A0*cos(c*lamda(m,n)*t)+B0*sin(c*lambda(m,n)*t)*bessel(m,lambda(m,n)*r)*&
+  ! displacement U_{mn}
+  Umn=(A0*cos(c*lambda(m,n)*t))+B0*sin(c*lambda(m,n)*t)*bessel(m,lambda(m,n)*r)*&
       (C0*cos(m*theta)+D0*sin(m*theta))
 
-end function umn
+end function Umn ! end of function Umn
  
