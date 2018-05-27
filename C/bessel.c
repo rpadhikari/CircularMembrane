b31537de45f0679e2969468a37a7e264f750b060
Rajendra Adhikari <rpadhikari@gmail.com>
2018-05-27 08:07:12 +0545

    Update bessel.c

--- C/bessel.c
+++ C/bessel.c
@@ -3,7 +3,7 @@ double pow(double, double);
 double bessel(int n, double x) {
   int i;
   double sum;
-  sum=1.0;
+  sum=0.0;
   for(i=0; i<=15; i++) {
     sum+= pow(-1.0, (double)(i+n))*pow(x/2.0, (double)n+2.0*i)/
           (fact(i)*fact(i+n));
